468153417d7d95b2db8419e0b71b50e0dab3ddf2
robertabramsky <rabramsky@wisc.edu>
2023-10-24 15:02:33 -0500

    Robert A - Added Maps functionality on main activity

--- app/src/main/java/com/cs407/safepath/MainActivity.java
+++ app/src/main/java/com/cs407/safepath/MainActivity.java
@@ -1,18 +1,44 @@
 package com.cs407.safepath;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Madison and move the camera
+        LatLng madison = new LatLng(43, -89);
+        mMap.addMarker(new MarkerOptions()
+                .position(madison)
+                .title("Marker in Madison"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(madison));
+    }
+}
 
     // Aidan testing a comment
     //Robert Test
-
-}
