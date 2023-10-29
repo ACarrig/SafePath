834964aa648aae800b09318bcc3e683d95e24ff1
mismail6 <mismail6@wisc.edu>
2023-10-29 00:11:29 -0500

    Created activities for Flagging and Settings. Edited Settings page. Both buttons are functional.

--- app/src/main/java/com/cs407/safepath/MainActivity.java
+++ app/src/main/java/com/cs407/safepath/MainActivity.java
@@ -4,16 +4,21 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.preference.PreferenceFragment;
+import android.view.View;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
 
     private GoogleMap mMap;
 
@@ -22,9 +27,26 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        //fragmentManager = getSupportFragmentManager();
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        FloatingActionButton fab1 = findViewById(R.id.settingsButton);
+        FloatingActionButton fab2 = findViewById(R.id.flagButton);
+        fab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSettings();
+            }
+        });
+
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFlaggingScreen();
+            }
+        });
     }
 
     @Override
@@ -38,6 +60,16 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .title("Marker in Madison"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(madison));
     }
+
+    public void openSettings() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+    public void openFlaggingScreen() {
+        Intent intent = new Intent(this, FlaggingActivity.class);
+        startActivity(intent);
+    }
+
 }
 
     // Aidan testing a comment
