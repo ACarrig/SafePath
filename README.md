9015f48fbf1a8732a089eae7d5f82035c1f3999a
acarrig <aidan@carrig.com>
2023-10-31 20:08:56 -0500

    User location + SearchBar

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Safe-Path
-An App made on Android Studio that helps users reach their destination safetly and efficiently.
+An App made on Android Studio that helps users reach their destination safely and efficiently.
+Implements Google Maps API
+Relies on user location permission to function properly
--- app/src/main/java/com/cs407/safepath/MainActivity.java
+++ app/src/main/java/com/cs407/safepath/MainActivity.java
@@ -2,35 +2,97 @@ package com.cs407.safepath;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.view.View;
+import android.widget.SearchView;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-
     private GoogleMap mMap;
+    private SearchView mapSearchView;
+
+    // For getting user location
+    private final int FINE_PERMISSION_CODE = 1;
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapSearchView = findViewById(R.id.mapSearch);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        //fragmentManager = getSupportFragmentManager();
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        // fragmentManager = getSupportFragmentManager();
+
+        // In the youtube series, this line as well as line 89 are uncommented, but in the
+        //     get user location tutorial, they are entirely removed
+        //     May need to figure out later.
+        //SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        mapSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                String userEnteredLocation = mapSearchView.getQuery().toString();
+                List<Address> addressList = null;
+
+                if (userEnteredLocation != null) {
+                    Geocoder geocoder = new Geocoder(MainActivity.this);
+
+                    // Assign the user's query from search bar to addressList
+                    try {
+                        addressList = geocoder.getFromLocationName(userEnteredLocation, 1);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    // If getting multiple results, add a for loop here and change maxResults ^^^
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(userEnteredLocation));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                }
+
+                return false;
+            }
+
+            // mapFragment.getMapAsync(this);
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
 
         FloatingActionButton fab1 = findViewById(R.id.settingsButton);
         FloatingActionButton fab2 = findViewById(R.id.flagButton);
@@ -54,23 +116,58 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap = googleMap;
 
         // Add a marker in Madison and move the camera
-        LatLng madison = new LatLng(43.073051, -89.401230);
-        mMap.addMarker(new MarkerOptions()
-                .position(madison)
-                .title("Marker in Madison"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(madison));
+
+        // Current Coordinates
+        LatLng usrPos = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        usrPos = new LatLng(43.073051, -89.401230); // hardcode location
+
+        // Setting the camera
+        mMap.addMarker(new MarkerOptions().position(usrPos).title("My Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(usrPos));
     }
 
     public void openSettings() {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
+
     public void openFlaggingScreen() {
         Intent intent = new Intent(this, FlaggingActivity.class);
         startActivity(intent);
     }
 
-}
+    private void getLastLocation() {
+        // Permission check
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-    // Aidan testing a comment
-    //Robert Test
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_PERMISSION_CODE);
+            return;
+        }
+
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    currentLocation = location;
+                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                    mapFragment.getMapAsync(MainActivity.this);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == FINE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            } else {
+                Toast.makeText(this, "Location Permission Denied \n " +
+                        "Please allow this permission so that the app can function correctly", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
